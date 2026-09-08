3288eb2eb6f44fad728f478e70b601f32d7a9e4b
Yusuf Tamer <yusuftamer12345678910@gmail.com>
2026-09-08 17:07:35 +0300

    updated dates

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:kilicbaran/boun-course-planner.git master:gh-pages
+git push -f https://github.com/ytakyol/boun-course-planner.git master:gh-pages
 
 cd -
